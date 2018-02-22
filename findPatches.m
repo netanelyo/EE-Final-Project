bf5b95503e781dfabbbd49c121c7d30496aaf17a
etanel yosephian <nati.yosephian@gmail.com>
2018-02-22 16:05:58 +0200

    findPatches.m almost complete (need to see what to do when features are not valid), predictLocation.m should be OK

--- findPatches.m
+++ findPatches.m
@@ -1,26 +1,34 @@
 function [ features ] = findPatches(features, Icurr, currTime)
-    envSize = 65;
-    shift   = 20;
     [rows, cols] = size(Icurr);
-    for i = 1:21
+    [~, numOfFeatures] = size(features);
+    tmpPosition = cell(1, numOfFeatures);
+    for i = 1:numOfFeatures
+        shift   = 20;
+        envSize = 65;
         feature = features(i);
         if (feature.lastSeen < currTime - 1)
-            % Predict location
-        else
-            % TODO: put in aux function
-            xPatch  = feature.pos(1);
-            xEnv    = max(1, xPatch - shift);
-            yPatch  = feature.pos(2);
-            yEnv    = max(1, yPatch - shift);
-            width   = min(cols, xEnv + envSize) - xEnv;
-            height  = min(rows, yEnv + envSize) - yEnv;
-            envRect = [xEnv, yEnv, width, height];
-            IcurrEnv = imcrop(Icurr, envRect);
-            [tmpPos, isValid] = [0, 0]; %tmp, valid = findPatch(feature.data, [xPatch - xEnv, yPatch - yEnv, width, height], IcurrEnv);
+            [ prediction, valid ] = predictLocation(features, i);
+            if (valid)
+                envSize = 89;
+                shift   = 31;
+                feature.pos(1:2) = prediction;
+            else
+                continue;
+            end
         end
-        something = checkConstraints(tmpPos, i); %TODO
+        envRect = setEnvironment(feature.pos(1) - shift, ...
+                                 feature.pos(2) - shift, ...
+                                 envSize, cols, rows);
+        IcurrEnv = imcrop(Icurr, envRect);
+        [tmpPosition(i), valid] = findPatch(feature.data, ...
+            [xPatch - xEnv, yPatch - yEnv, width, height], IcurrEnv);
     end
-
-
+    
+    %% TODO: this is good if all are valid!
+    for i = 1:numOfFeatures
+        features(i).data        = imcrop(Icurr, tmpPosition(i));
+        features(i).pos         =  tmpPosition(i);
+        features(i).lastSeen    =  currTime;
+    end
+%     something = checkConstraints(tmpPos, i); % TODO: implement
 end
-
